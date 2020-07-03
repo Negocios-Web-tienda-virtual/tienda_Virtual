7a5105e56965f998ba839277bc760071dd673117
Marpol96 <51094297+Marpol96@users.noreply.github.com>
2020-07-03 17:15:41 -0600

    autenticador para el modulo de administrador

---
+++ controllers/authAdminController.js
@@ -0,0 +1,34 @@
+// Importar passport
+const passport = require("passport");
+
+// Importar el modelo de administrador
+const admin = require("../models/administradorVS");
+
+// verificar si el administrador se puede autenticar con sus 
+const Sequelize = require("sequelize");
+
+// verificar si el administrador puede autenticar
+exports.autenticarAdmin = passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/inicio_sesion_admin",
+    badRequestMessage: "debes ingresar tu correo, contraseña y token",
+    failureFlash: true,
+});
+
+// cerrar la sesión del administrador actual
+// una vez cerrada la sesión del administrador actual le enviamos la vista de iniciar sesión
+exports.cerrarSesionAdmin = (req, res, next) => {
+    req.session.destroy(() => {
+        res.redirect("/inicio_sesion_admin");
+    })
+};
+
+//verificar si el administrador esta autenticado
+exports.adminAutenticado = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+
+    //si el administrador no esta autenticado, iniciar sesión
+    return res.redirect("/inicio_sesion_admin");
+};
