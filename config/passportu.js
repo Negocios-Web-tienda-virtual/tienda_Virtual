c946361f2620c42f95d9525ef7576bad550c2900
sor7o <angelsorto330@gmail.com>
2020-07-15 01:26:22 -0600

    Corrigiendo error

--- config/passportu.js
+++ config/passportu.js
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
 const passport = require("passport");
 
 const LocalStrategy = require("passport-local");
 
+=======
+const passport = require("passport");
+
+const LocalStrategy = require("passport-local");
+
+>>>>>>> dae5de2... actualizacion vista agregar productos
 const User = require()
