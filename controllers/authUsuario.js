0074a55e5b0d94dbd8fb15e8d3b6cf64bfbc4b2c
Martin Leiva <leivamartin1996@gmail.com>
2020-07-15 12:55:15 -0600

    Agregando mensajes al authController

--- controllers/authUsuario.js
+++ controllers/authUsuario.js
@@ -1,35 +1,43 @@
 const passport = require("passport");
 
 const Usuario = require("../models/Usuario");
- 
 
-exports.autenticarUsuario = passport.authenticate("local",{
+
+exports.autenticarUsuario = passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/inicio_sesion",
     badRequestMessage: "debes ingresar tu correo y contraseña",
     failureFlash: true,
 });
 
-exports.cerrarSesion =(req, res, next)=>{
-    req.session.destroy(()=>{
+exports.cerrarSesion = (req, res, next) => {
+    req.session.destroy(() => {
         res.redirect("/inicio_sesion");
     })
 };
 
-exports.usuarioAutenticado = (req, res, next)=>{
+exports.usuarioAutenticado = (req, res, next) => {
     if (req.isAuthenticated()) {
         return next();
     }
 
     return res.redirect("/inicio_sesion");
+    mensajes.push({
+        error: "No estas autenticado, intenta nuevamente.",
+        type: "alert",
+    });
 };
 
-exports.usuarioAdmin = (req, res, next)=>{
+exports.usuarioAdmin = (req, res, next) => {
     const usuario = res.locals.Usuario;
-    if(usuario.nivelUsuario == "administrador"){
+    if (usuario.nivelUsuario == "administrador") {
         console.log(usuario.nivelUsuario);
         return next();
     }
 
     return res.redirect("/");
+    mensajes.push({
+        error: "No estas autenticado, intenta nuevamente.",
+        type: "alert",
+    });
 };
