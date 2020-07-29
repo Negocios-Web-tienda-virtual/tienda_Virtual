d53ce04467e3023088a6d7ffe5e85e5c082b18c6
sor7o <angelsorto330@gmail.com>
2020-07-28 18:37:55 -0600

    mostrar total a pagar

--- controllers/carritoController.js
+++ controllers/carritoController.js
@@ -7,6 +7,13 @@ exports.formularioCarrito = (req, res, next) => {
 };
 exports.mostrarProductos = async(req, res, next) => {
     const usuario = res.locals.Usuario;
+    var valor=0;
+    var sum=0;
+    var subtotalantedepagar=0;
+    var imp=0;
+    var total=[];
+    var impuesto=[];
+    var subtotal=[];
     try {
         Pedidos.findAll({
             where: {
@@ -15,35 +22,19 @@ exports.mostrarProductos = async(req, res, next) => {
         }).then(function(pedidos) {
             pedidos = pedidos.map(function(pedido) {
                 pedido.dataValues.fecha = moment(pedido.dataValues.fecha).fromNow();
-                console.log(pedido.dataValues.precio);
-                const totales = calculartotales(pedido.dataValues.precio);
+                valor=pedido.dataValues.precio;
+                sum+=valor;
                 return pedido;
             });
-            res.render("Carrito", { pedidos, layout: "auth" });
+            imp=sum*0.15;
+            subtotalantedepagar=sum-imp;
+            total.push({tot: parseFloat(sum).toFixed(2)});
+            impuesto.push({imp:parseFloat(imp).toFixed(2)});
+            subtotal.push({sub: parseFloat(subtotalantedepagar).toFixed(2)});
+            res.render("Carrito", {impuesto,subtotal, total ,pedidos, layout: "auth" });
         });
 
     } catch (error) {
         console.log(error);
     }
-};
-
-const calculartotales = (price) => {
-    let totales = [];
-    let precio = price;
-    let subtot = 0;
-    let subtot1 = 0
-    let imp = 0;
-    let tot = 0;
-    subtot1 = subtot1 + precio;
-    imp = subtot1 * 0.15;
-    subtot = subtot1 - imp;
-    tot = subtot + imp;
-
-    totales.push({
-        subtot,
-        imp,
-        tot,
-    });
-    console.log(totales);
-    return totales;
 };
