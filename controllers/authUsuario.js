780adba8c4e1a9e5fab15111a36eabf7c7d4febc
leivamartin1996@gmail.com <leivamartin1996@gmail.com>
2020-07-30 03:03:30 -0600

    importacion de crypto ( Restablecer contraseña. beta)

--- controllers/authUsuario.js
+++ controllers/authUsuario.js
@@ -2,6 +2,10 @@ const passport = require("passport");
 
 const Usuario = require("../models/Usuario");
 
+//importar crypto
+const crypto = require("crypto");
+//importar la configuración de envio de correo electrónico
+const enviarCoreo = require("../helpers/email");
 
 exports.autenticarUsuario = passport.authenticate("local", {
     successRedirect: "/",
@@ -44,19 +48,60 @@ exports.usuarioAdmin = (req, res, next) => {
     });
 };
 
-exports.usuarioCliente = (req, res, next)=>{
+exports.usuarioCliente = (req, res, next) => {
     const usuario = res.locals.Usuario;
     const mensajes = [];
-    if(usuario.nivelUsuario == "cliente"){
-    return next();
-    }
-    else{
+    if (usuario.nivelUsuario == "cliente") {
+        return next();
+    } else {
         mensajes.push({
             error: "Debes registrarte para poder agregar pedidos",
             alert: "alert-warning",
         });
         res.redirect("/");
     }
-    
-};
 
+};
+exports.enviarToken = async(req, res, next) => {
+    // Verificar si existe el usuario
+    const { email } = req.body;
+    const usuario = await Usuario.findOne({
+        where: {
+            email,
+        },
+    });
+
+    // Si el usuario no existe
+    if (!usuario) {
+        req.flash("error", "¡Este usuario no está registrado!");
+        res.redirect("/restablecerPassword");
+    }
+
+    // Si el usuario existe
+    // Generar un token único con una fecha de expiración
+    usuario.token = crypto.randomBytes(20).toString("hex");
+    usuario.expiration = Date.now() + 3600000;
+
+    // Guardar el token y la fecha de validez
+    await usuario.save();
+
+    // URL de reestablecer contraseña
+    const resetUrl = `http://${req.headers.host}/reestablecerPassword/${usuario.token}`;
+
+    // Enviar el correo electrónico al usuario con el link que contiene
+    // el token generado
+    await enviarCorreo.enviarCorreo({
+        usuario,
+        subject: "Reestablece tu contraseña de usuario",
+        resetUrl,
+        vista: "email_reestablecer",
+        text: "Has solicitado reestablecer tu contraseña de Taskily! Autoriza el contenido HTML.",
+    });
+
+    // Redireccionar al usuario al inicio de sesión
+    req.flash(
+        "success",
+        "Se envió un enlace para reestablecer tu contraseña a tu correo electrónico"
+    );
+    res.redirect("/iniciar_sesion");
+};
