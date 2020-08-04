c68831172417d07b6dfe89606512e0108f5c5101
sor7o <angelsorto330@gmail.com>
2020-08-03 23:52:12 -0600

    actualizacion de vistas pedidos, productos, inicio, menu y agregar productos

--- controllers/inicioController.js
+++ controllers/inicioController.js
@@ -1,3 +1,5 @@
-exports.formularioInicio = (req, res, next)=>{
-    res.render("inicio", { layout : "auth"});
+const usuario = require("../models/Usuario");
+exports.formularioInicio = (req, res, next) => {
+    const usuario = res.locals.Usuario;
+    res.render("inicio", { layout: "auth", userLog: usuario.nivelUsuario == "administrador" || usuario.nivelUsuario == "cliente" ? true : false });
 };
